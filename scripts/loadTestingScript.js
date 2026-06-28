5af9a5c9719f54eb66f1bcab8f6ca7481013b903
Keizh <krishnamulay05@gmail.com>
2026-06-28 11:34:21 +0530

    Update loadTestingScript.js

--- scripts/loadTestingScript.js
+++ scripts/loadTestingScript.js
@@ -3,19 +3,29 @@ import { check, sleep } from 'k6'
 
 export const options = {
     stages: [
-        { duration: '5s', target: 100 },
-        { duration: '15s', target: 100 },
+        { duration: '5s', target: 5 },
+        { duration: '5s', target: 10 },
         { duration: '5s', target: 0 }
     ]
 }
 
 export default function () {
-    const targetUrl = 'http://localhost:8000/url/redirect?short_code=12i0T1c2';
 
-    const res = http.get(targetUrl, { redirects: 0 })
+    const targetUrl1 = 'http://localhost:8000/url/redirect?short_code=Oq7y9sY0aR';
+    const targetUrl2 = 'http://localhost:8000/url/shorten';
 
-    check(res, {
+    const res1 = http.get(targetUrl1, { redirects: 0 })
+    check(res1, {
         'succesfull redirect': (r) => r.status == 307,
+    })
+
+    const randomString = Math.random().toString(36).substring(2);
+    const res2 = http.post(targetUrl2, JSON.stringify({ long_url: `https://www.example.com/${randomString}` }), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    check(res2, {
+        'successfull Insert': (r) => r.status == 201
     })
 
     sleep(0.05)
